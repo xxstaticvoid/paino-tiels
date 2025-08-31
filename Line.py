b3e9a89bdc85b08cb88fb65a3561fa39372d9520
xxstaticvoid <josephebersole404@gmail.com>
2025-08-31 10:02:38 -0500

    Update Line.py

--- Line.py
+++ Line.py
@@ -5,10 +5,11 @@ class Line():
     def __init__(self):
     
         self.color = "purple"
-        self.rect = pygame.Rect(-10, (500 * 9) // 10, 350 + 40, 4)
+        self.rect = pygame.Rect(-10, (500 * 9) // 10, 350 + 40, 4) ## (x, y, width, height)
 
     def update(self):
         pass
     
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.rect)
+
